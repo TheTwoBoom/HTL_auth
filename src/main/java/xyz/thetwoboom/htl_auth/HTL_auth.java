fc776a68ee496eb36b4fae4d668f16a438b164c4
TheTwoBoom <87650485+TheTwoBoom@users.noreply.github.com>
2024-11-03 17:53:24 +0100

    Update HTL_auth.java

--- src/main/java/xyz/thetwoboom/htl_auth/HTL_auth.java
+++ src/main/java/xyz/thetwoboom/htl_auth/HTL_auth.java
@@ -1,4 +1,23 @@
 package xyz.thetwoboom.htl_auth;
+/*
+
+    HTLAuth: Easy Authentication for Minecraft via Microsoft Entra ID
+    Copyright (C) 2024 TheTwoBoom
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
